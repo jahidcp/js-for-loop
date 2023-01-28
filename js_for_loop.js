82aea0f8c2133184327568567cf37722c6a81a7f
jahidcp <programmingloverjahid@gmail.com>
2023-01-28 19:57:11 +0600

    some practise for loop

---
+++ js_for_loop.js
@@ -0,0 +1,24 @@
+// While for example
+var num = 0;
+while(num <= 10){
+    console.log("I will start in LOOP");
+    num += 1;
+}
+
+// For Loop example
+
+for(var num = 0; num <= 10; num += 1){
+    console.log("I will run For LOOP");
+}
+
+// Odd value declare for loop
+
+for(var i = 1; i <= 10; i += 2){
+    console.log(i);
+}
+
+// Even value declare for loop
+
+for(var i = 0; i <= 20; i +=2){
+    console.log(i);
+}
